6b6fe3c099137e7bd8b60d1530bc252287339fc3
amidgard0 <92100001+amidgard0@users.noreply.github.com>
2021-11-30 17:01:58 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -24,4 +24,30 @@ function tabs(tabsItems, tabsContentItems) {
 
 tabs('.about-us__item-tabs--1', '.about-us__item-tabs-content-item--1');
 tabs('.about-us__item-tabs--2', '.about-us__item-tabs-content-item--2');
+$(function () {
+  $('.our__clietns').on('click', function (e) {
+    $('html,body').stop().animate({
+      scrollTop: $('#our__clientss').offset().top
+    }, 1000);
+    e.preventDefault();
+  });
+  $('.our__contact').on('click', function (e) {
+    $('html,body').stop().animate({
+      scrollTop: $('#our__contact').offset().top
+    }, 1000);
+    e.preventDefault();
+  });
+});
+var close = document.querySelector('.close'),
+    menuContent = document.querySelector('.menu__mobile'),
+    openMenu = document.querySelector('.burger__menu');
+var body = document.body;
+openMenu.addEventListener('click', function () {
+  menuContent.classList.add('menu__mobile-active');
+  body.classList.add('overFlow');
+});
+close.addEventListener('click', function () {
+  menuContent.classList.remove('menu__mobile-active');
+  body.classList.remove('overFlow');
+});
 //# sourceMappingURL=main.js.map
