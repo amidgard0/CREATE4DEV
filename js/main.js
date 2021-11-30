d84d1b06fc20f48fcda78c4f4488fe5a3bd51213
amidgard0 <92100001+amidgard0@users.noreply.github.com>
2021-11-30 13:51:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 "use strict";
 "use strict";
 "use strict";
+
+function tabs(tabsItems, tabsContentItems) {
+  var tabBtn = document.querySelectorAll(tabsItems),
+      tabItems = document.querySelectorAll(tabsContentItems);
+  tabBtn.forEach(function (item) {
+    item.addEventListener('click', function () {
+      var currentBtn = item;
+      var tabId = currentBtn.getAttribute('data-tab');
+      var currentTab = document.querySelector(tabId);
+      tabBtn.forEach(function (item) {
+        item.classList.remove('active__tab-item');
+      });
+      tabItems.forEach(function (item) {
+        item.classList.remove('active-tab');
+      });
+      currentTab.classList.add('active-tab');
+      currentBtn.classList.add('active__tab-item');
+    });
+  });
+}
+
+tabs('.about-us__item-tabs--1', '.about-us__item-tabs-content-item--1');
+tabs('.about-us__item-tabs--2', '.about-us__item-tabs-content-item--2');
 //# sourceMappingURL=main.js.map
